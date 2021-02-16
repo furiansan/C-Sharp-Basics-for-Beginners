b564a8f26bc489f6779c19a2c9d8abac80db418e
furian <jvr415@gmail.com>
2021-02-15 19:42:13 -0500

    Primer ejercicio

---
+++ Conditional 1/Conditional 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Conditional_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero:");
+            string date = Console.ReadLine();
+            var numberValidation = int.TryParse(date, out int number);
+            if (numberValidation && number>0 && number<=10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
