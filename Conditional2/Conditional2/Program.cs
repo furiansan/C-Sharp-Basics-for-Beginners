4bc0e5d262bab33d720a1ff4d40e21e270e471f0
furian <jvr415@gmail.com>
2021-02-16 15:24:33 -0500

    Segundo ejercicio

---
+++ Conditional2/Conditional2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Conditional2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese limite de velocidad: ");
+            var limiteVelocidad =int.Parse(Console.ReadLine());
+            Console.Write("Ingrese velocidad del automovil: ");
+            var velocidadAuto = int.Parse(Console.ReadLine());
+
+            if (velocidadAuto<limiteVelocidad)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                const int kmPorPuntos = 5;
+                var puntos = (velocidadAuto - limiteVelocidad) / kmPorPuntos;
+                if (puntos>12)
+                {
+                    Console.WriteLine("Licencia suspendida");
+                }
+                else
+                {
+                    Console.WriteLine("Puntos en la licencia: {0}",puntos);
+                }
+            }
+
+        }
+    }
+}
